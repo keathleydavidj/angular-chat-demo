e473e53bee19b6912062ac6e0d394dc75f88fa92
David Keathley <keathleydavidj@gmail.com>
2024-09-26 14:37:34 -0700

    add dummy "sign-in" feature

--- src/app/chat-container/chat-container.component.ts
+++ src/app/chat-container/chat-container.component.ts
@@ -22,6 +22,8 @@ export class ChatContainerComponent implements AfterViewChecked {
     {userID: 4, name: 'Gabbey'}
   ];
 
+  currentUser: User = this.users[3];
+
   messages: ChatMessage[] = [
     {id: 1, user: this.users[0], content: 'Hello everyone!', timestamp: new Date()},
     {id: 2, user: this.users[1], content: 'Hi Kevin, how are you?', timestamp: new Date()},
@@ -33,7 +35,7 @@ export class ChatContainerComponent implements AfterViewChecked {
   addMessage(content: string) {
     const newMessage: ChatMessage = {
       id: this.messages.length + 1,
-      user: this.users[Math.floor(Math.random() * this.users.length )], // Random user
+      user: this.currentUser, // Random user
       content: content,
       timestamp: new Date()
     }
@@ -55,6 +57,13 @@ export class ChatContainerComponent implements AfterViewChecked {
       console.log(`scrolling to new message at ${element.scrollHeight}`)
     } catch (err) {
       console.error('Error scrolling to bottom: ', err);
+    }
+  }
+
+  onUserChange(userID: string) {
+    const index = parseInt(userID);
+    if (!isNaN(index) && index >= 0 && index < this.users.length) {
+      this.currentUser = this.users[index];
     }
   }
 }
