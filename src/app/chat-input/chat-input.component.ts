0f5308e9956e65cc55abe181a4f4eac38239d173
David Keathley <keathleydavidj@gmail.com>
2024-09-27 12:55:45 -0700

    add keystroke handling for dropdown selection

--- src/app/chat-input/chat-input.component.ts
+++ src/app/chat-input/chat-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgIf, NgForOf } from '@angular/common';
 
@@ -14,11 +14,13 @@ import { User } from '../models/chat.model';
 export class ChatInputComponent {
   @Input() users: User[] = [];
   @Output() sendMessage = new EventEmitter<string>();
+  @ViewChild('messageInput') messageInput!: ElementRef
 
   newMessage: string = '';
   showMentionDropdown: boolean = false;
   filteredUsers: User[] = [];
   mentionFilter: string = '';
+  selectedIndex: number = -1;
 
   onInputChange(event: any) {
     const cursorPosition = event.target.selectionStart;
@@ -31,6 +33,7 @@ export class ChatInputComponent {
       this.showMentionDropdown = true;
       this.mentionFilter = lastWord.slice(1).toLowerCase();
       this.filterUsers();
+      this.selectedIndex = -1;
     } else {
       this.showMentionDropdown = false;
     }
@@ -46,6 +49,42 @@ export class ChatInputComponent {
     const cursorPosition = this.newMessage.lastIndexOf('@');
     this.newMessage = `${this.newMessage.slice(0, cursorPosition)}@${userName} `;
     this.showMentionDropdown = false;
+    this.focusInput();
+  }
+
+  onKeyDown(event: KeyboardEvent) {
+    if (!this.showMentionDropdown) return;
+
+    switch(event.key) {
+      case 'ArrowDown':
+        event.preventDefault();
+        this.selectedIndex = this.selectedIndex + 1 < this.filteredUsers.length
+          ? this.selectedIndex + 1
+          : 0;
+        break;
+      case 'ArrowUp':
+        event.preventDefault();
+        this.selectedIndex = this.selectedIndex > 0
+          ? this.selectedIndex - 1
+          : this.filteredUsers.length - 1;
+        break;
+      case 'Tab':
+        event.preventDefault();
+        if (this.selectedIndex >= 0) {
+          this.selectMention(this.filteredUsers[this.selectedIndex].name);
+        }
+        break;
+      case 'Escape':
+        this.showMentionDropdown = false;
+        break;
+    }
+  }
+
+  focusInput() {
+    setTimeout(() => {
+      this.messageInput.nativeElement.focus();
+      this.messageInput.nativeElement.selectionStart = this.messageInput.nativeElement.selectionEnd = this.newMessage.length;
+    }, 0);
   }
 
   onSendMessage() {
